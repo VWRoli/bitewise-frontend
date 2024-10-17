8f2e5c70ee53a976f39cc947bb39f3f520552042
Roland <fustroli@gmail.com>
2024-10-17 08:04:18 +0200

    feat: show all ingredient data on meal card

--- app/(modules)/dashboard/(pages)/meals/components/MealCard.tsx
+++ app/(modules)/dashboard/(pages)/meals/components/MealCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardContent, Typography } from '@mui/material';
+import { Card, CardContent, Grid2, Typography } from '@mui/material';
 import { IMeal } from '@/app/(modules)/dashboard/(pages)/meals/interfaces';
 
 interface IProps {
@@ -12,10 +12,55 @@ const MealCard: React.FC<IProps> = ({ meal }) => {
         <Typography gutterBottom variant="h5" component="div">
           {meal.name}
         </Typography>
-        {meal.mealIngredients.map((mealIngredient, i) => (
-          <Typography key={i} variant="body2" color="textSecondary">
-            {mealIngredient.ingredientName} - {mealIngredient.quantity}
-          </Typography>
+        {meal.mealIngredients.map((mealIngredient) => (
+          <article key={mealIngredient.id}>
+            <Typography variant="body2" color="textSecondary">
+              {mealIngredient.ingredientName} - {mealIngredient.quantity} *{' '}
+              {mealIngredient.unit}
+            </Typography>
+            <Grid2 container spacing={2}>
+              <Grid2>
+                <Typography variant="body2" color="textSecondary">
+                  Calories: {mealIngredient.calories} kcal
+                </Typography>
+              </Grid2>
+              <Grid2>
+                <Typography variant="body2" color="textSecondary">
+                  Protein: {mealIngredient.protein}g
+                </Typography>
+              </Grid2>
+              <Grid2>
+                <Typography variant="body2" color="textSecondary">
+                  Total Fat: {mealIngredient.totalFat}g
+                </Typography>
+              </Grid2>
+              <Grid2>
+                <Typography variant="body2" color="textSecondary">
+                  Saturated Fat: {mealIngredient.saturatedFat}g
+                </Typography>
+              </Grid2>
+              <Grid2>
+                <Typography variant="body2" color="textSecondary">
+                  Total Carbohydrates: {mealIngredient.totalCarbohydrates}g
+                </Typography>
+              </Grid2>
+              <Grid2>
+                <Typography variant="body2" color="textSecondary">
+                  Sugar: {mealIngredient.sugar}g
+                </Typography>
+              </Grid2>
+              <Grid2>
+                <Typography variant="body2" color="textSecondary">
+                  Dietary Fiber: {mealIngredient.dietaryFiber}g
+                </Typography>
+              </Grid2>
+              <Grid2>
+                <Typography variant="body2" color="textSecondary">
+                  Unit: {mealIngredient.unit}
+                </Typography>
+              </Grid2>
+            </Grid2>
+          </article>
         ))}
       </CardContent>
     </Card>
--- app/(modules)/dashboard/(pages)/meals/interfaces/meal-ingredient.interface.ts
+++ app/(modules)/dashboard/(pages)/meals/interfaces/meal-ingredient.interface.ts
@@ -1,6 +1,21 @@
+import { EUnit } from '@/app/(modules)/dashboard/(pages)/ingredients/enums';
 import { ICreateMealIngredient } from '@/app/(modules)/dashboard/(pages)/meals/interfaces';
 
 export interface IMealIngredient extends ICreateMealIngredient {
   id: number;
   ingredientName: string;
+  calories: number;
+  protein: number;
+  totalCarbohydrates: number;
+  dietaryFiber: number;
+  sugar: number;
+  totalFat: number;
+  saturatedFat: number;
+  deleteTimeStamp: null;
+  name: string;
+  price: number;
+  unit: EUnit;
+  updateTimeStamp: string;
+  createTimeStamp: string;
+  userId: number;
 }
