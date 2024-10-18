70254c87bb87fbbc36a5ea71e76c6d68e739ee0d
Roland <fustroli@gmail.com>
2024-10-18 09:40:33 +0200

    feat: add meal plan cards

--- app/(modules)/dashboard/(pages)/meal-plans/components/MealPlanCard.tsx
+++ app/(modules)/dashboard/(pages)/meal-plans/components/MealPlanCard.tsx
@@ -1,4 +1,6 @@
 import { IMealPlan } from '@/app/(modules)/dashboard/(pages)/meal-plans/interfaces';
+import MealCardSum from '@/app/(modules)/dashboard/(pages)/meals/components/MealCardSum';
+import { Typography } from '@mui/material';
 import React from 'react';
 
 interface IProps {
@@ -10,9 +12,15 @@ const MealPlanCard = ({ mealPlan }: IProps) => {
       {mealPlan.meals.map((meal) => (
         <div key={meal.id}>
           <p>{meal.name}</p>
-          {meal.mealIngredients.map((ingredient) => (
-            <div key={ingredient.id}>{ingredient.id}</div>
+          {meal.mealIngredients.map((mealIngredient) => (
+            <article key={mealIngredient.id}>
+              <Typography variant="body2" color="textSecondary">
+                {mealIngredient.ingredientName} - {mealIngredient.quantity} *{' '}
+                {mealIngredient.unit}
+              </Typography>
+            </article>
           ))}
+          <MealCardSum meal={meal} />
         </div>
       ))}
     </div>
